8f24a88f8b0950831457901299656f82d3023341
Patriciafo48 <patricia.fo@icloud.com>
2025-02-11 16:55:46 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,6 +12,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var plymouthMarker = L.marker([50.3755, -4.1427]).addTo(map);
     plymouthMarker.bindPopup("<b>Plymouth, England</b><br>Ausgangspunkt der Expedition (Juli 1772)");
 
+    // Marker für Madera
+    var madeiraMarker = L.marker([32.7607, -16.9595]).addTo(map);
+    madeiraMarker.bindPopup("<b>Madeira</b><br>Erste Zwischenstation für Vorräte");
+
 
     map.setView([0, 0], 2); // Zentriere Karte auf 0,0
 });
