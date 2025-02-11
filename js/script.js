cbfac4a34470b29866e7b1ece3f2d6ae09b7333d
Patriciafo48 <patricia.fo@icloud.com>
2025-02-11 16:10:09 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var map = L.map('interactive-map-container').setView([0, 0], 2); // Startposition und Zoom global
 
     // 2. Kartenkachelebene (Tile Layer) hinzufügen (OpenStreetMap)
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://tile.openstreetmap.org/{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
     // Marker für Plymouth
@@ -21,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Marker für Kapstadt (Kap der Guten Hoffnung)
     var capetownMarker = L.marker([-33.9249, 18.4241]).addTo(map);
     capetownMarker.bindPopup("<b>Kapstadt, Südafrika</b><br>Aufenthalt zum Auffüllen der Vorräte und Reparaturen (Okt.-Nov. 1772)<br><a href='https://de.wikipedia.org/wiki/Kapstadt' target='_blank'>Wikipedia</a>");
+
 
     // Zentrum der Karte auf Europa setzen, damit Plymouth und Madera sichtbar sind
     map.setView([40, -10], 4); // Angepasste Zentrumskoordinaten und Zoom
