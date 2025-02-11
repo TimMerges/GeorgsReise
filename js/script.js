176dee4871a1bd45a489107eee622c5a3a81818b
Patriciafo48 <patricia.fo@icloud.com>
2025-02-11 17:17:28 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,11 +7,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Marker für Plymouth
     var plymouthMarker = L.marker([50.3755, -4.1427]).addTo(map);
-    plymouthMarker.bindPopup("<b>Plymouth, England</b><br>Ausgangspunkt der Expedition (Juli 1772)<br><a href='https://www.wikidata.org/wiki/Q43382' target='_blank'>Wikidata: Plymouth</a>");
+    plymouthMarker.bindPopup("<b>Plymouth, England</b><br>Ausgangspunkt der Expedition (Juli 1772)<br><a href='https://www.wikidata.org/wiki/Q47525' target='_blank'>Wikidata: Plymouth</a>");
 
     // Marker für Madera
     var madeiraMarker = L.marker([32.7607, -16.9595]).addTo(map);
-    madeiraMarker.bindPopup("<b>Madeira</b><br>Erste Zwischenstation für Vorräte<br><a href='https://www.wikidata.org/wiki/Q26253' target='_blank'>Wikidata: Madeira</a>");
+    madeiraMarker.bindPopup("<b>Madeira</b><br>Erste Zwischenstation für Vorräte<br><a href='https://www.wikidata.org/wiki/Q17712' target='_blank'>Wikidata: Madeira</a>");
+
+    // Marker für Kapstadt (Kap der Guten Hoffnung) mit Wikidata-Link
+    var capetownMarker = L.marker([-33.9249, 18.4241]).addTo(map);
+    capetownMarker.bindPopup("<b>Kapstadt, Südafrika</b><br>Aufenthalt zum Auffüllen der Vorräte und Reparaturen (Okt.-Nov. 1772)<br><a href='https://www.wikidata.org/wiki/Q5465' target='_blank'>Wikidata: Kapstadt</a>");
 
 
     map.setView([0, 0], 2);
